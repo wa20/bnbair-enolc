cf75c72eca78501d48d9ad275690706b032e3ec9
wael <78425538+wa20@users.noreply.github.com>
2024-05-13 16:36:57 +0100

    hero banner created

---
+++ airbnb-app/src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image';
+
+function Hero() {
+  return (
+    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+      <Image
+        src='https://links.papareact.com/0fm'
+        layout='fill'
+        objectFit='cover'
+      />
+
+      <div className='absolute top-1/2 w-full text-center'>
+         <p className='text-sm sm:text-lg font-bold'>Not sure where to go? Perfect.</p>
+
+         <button className='text-purple-500 bg-white rounded-full px-10 py-4 shadow-md  my-3 hover:shadow-xl active:scale-90 duration-150'>
+            I'm flexable
+         </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
