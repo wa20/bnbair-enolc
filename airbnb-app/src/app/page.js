064c941ac05a2598fcdd56af28b6aa41958c5eb1
wael <78425538+wa20@users.noreply.github.com>
2024-05-14 12:53:05 +0100

    page file added getStaticProps()

--- airbnb-app/src/app/page.js
+++ airbnb-app/src/app/page.js
@@ -2,8 +2,15 @@ import Image from "next/image";
 import Head from 'next/head';
 import Header from "../components/Header";
 import Hero from "@/components/Hero";
+// import { InferGetStaticPropsType, GetStaticProps } from 'next'
 
-export default function Home() {
+
+const Home = async ({ exploreData }) => {
+
+  // const staticData = await fetch(`https://...`, { cache: 'force-cache' })
+
+  // const exploreData = await fetch('https://links.papareact.com/pyp' , { cache: 'force-cache' })
+
   return (
     <div className="">
 
@@ -20,7 +27,18 @@ export default function Home() {
 
       {/* Main /section */}
       
-      
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+            <h2 className="text-4xl font-semibold pb-5"> Explore Nearby </h2>
+
+            {exploreData?.map(item => (
+              <h1 key={index}>{item.location}</h1>
+            ))}
+
+        </section>
+
+          {/* pull data from server */}
+      </main>
 
 
 
@@ -28,4 +46,17 @@ export default function Home() {
       
     </div>
   );
+}
+
+export default Home
+
+export async function getStaticProps() {
+  // Fetch data from an API
+  const res = await fetch('https://links.papareact.com/pyp');
+  const exploreData = await res.json();
+
+  // Pass data to the page component as props
+  return {
+    props: { exploreData },
+  };
 }
