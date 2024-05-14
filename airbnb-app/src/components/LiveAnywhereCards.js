f25e46ddd3baf48910171bc63cdfee376a168960
wael <78425538+wa20@users.noreply.github.com>
2024-05-14 15:26:43 +0100

    updated to exploreCard and liveAnywhereCards

--- airbnb-app/src/components/LiveAnywhereCards.js
+++ airbnb-app/src/components/LiveAnywhereCards.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Image from "next/image";
 
-const LiveAnywhereCards = ({ img, title }) => {
+const LiveAnywhereCards = ({ img, title}) => {
   return (
-    <div>
-      <div className="relative h-18 w-18">
+    <div className="m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:scale-105 transition transform duration-300 ease-out">
+      <div className="relative h-80 w-80">
         <Image
           src={img}
           alt={title}
           layout="fill"
           objectFit="contain"
-          className="rounded-lg"
+          className="rounded-xl"
         />
       </div>
       <div>
-        <h3 className="text-gray-500">{title}</h3>
+        <h3 className="text-2xl mt-3 text-gray-500">{title}</h3>
       </div>
     </div>
   );
