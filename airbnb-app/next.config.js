0a780e5d4167179b9b581bc6ce6d4bdb61c010c5
wael <78425538+wa20@users.noreply.github.com>
2024-05-14 12:54:15 +0100

    config file updates

--- airbnb-app/next.config.js
+++ airbnb-app/next.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-    images: {
-        domains: ['links.papareact.com']
-    }
-}
+const path = require('path');
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+        images: {
+            domains: ['links.papareact.com']
+        },
+        webpack: (config, { defaultLoaders }) => {
+            config.resolve.alias['@'] = path.resolve(__dirname, 'src/app');
+            return config;
+          },
+  }
+   
+module.exports = nextConfig
