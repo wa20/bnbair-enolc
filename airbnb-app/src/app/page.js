220ff6ccea58ce5608a7cbc4c06cfe38f335e864
wael <78425538+wa20@users.noreply.github.com>
2024-05-14 13:12:42 +0100

    formatting amends

--- airbnb-app/src/app/page.js
+++ airbnb-app/src/app/page.js
@@ -1,60 +1,58 @@
-import Image from 'next/image'
-import Head from 'next/head';
+import Image from "next/image";
+import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "@/components/Hero";
-import ExploreCards from '@/components/ExploreCards';
-
+import ExploreCards from "@/components/ExploreCards";
 
 async function getData() {
-  const res = await fetch('https://www.jsonkeeper.com/b/4G1G', { cache: 'force-cache' });
-  const data = await res.json()
-  return data
+  const res = await fetch("https://www.jsonkeeper.com/b/4G1G", {
+    cache: "force-cache",
+  });
+  const data = await res.json();
+  return data;
 }
 
 const Page = async () => {
-
-  const exploreData = await getData()
+  const exploreData = await getData();
 
   return (
     <div className="">
-
       <Head>
-          <title>Airbnb</title>
-          <link rel='icon' href="/favicon.ico" />
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Header Section*/}
-      <Header/>
+      <Header />
 
       {/* Banner Section*/}
-      <Hero/>
+      <Hero />
 
       {/* Main /section */}
-      
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
-            <h2 className="text-4xl font-semibold pb-5"> Explore Nearby </h2>
+          <h2 className="text-4xl font-semibold pb-5"> Explore Nearby </h2>
 
-            {exploreData?.map(({img, distance, location}) => (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            {exploreData?.map(({ img, distance, location }) => (
               <ExploreCards
-              key={img} 
-              img={img} 
-              distance={distance} 
-              location={location} 
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
               />
             ))}
-
+          </div>
         </section>
 
-          {/* pull data from server */}
+        <section>
+        <h2 className="text-4xl font-semibold py-8"> Live Anywhere </h2>
+        
+
+        </section>
       </main>
-
-
-
-
-      
     </div>
   );
-}
+};
 
-export default Page
+export default Page;
