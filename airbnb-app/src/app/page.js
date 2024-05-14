84332c1b60fe992c58df3a515ffa28b7a551c9be
wael <78425538+wa20@users.noreply.github.com>
2024-05-14 15:28:23 +0100

    added new components to the page

--- airbnb-app/src/app/page.js
+++ airbnb-app/src/app/page.js
@@ -3,17 +3,33 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "@/components/Hero";
 import ExploreCards from "@/components/ExploreCards";
+import LiveAnywhereCards from "../components/LiveAnywhereCards";
+import FooterHero from "@/components/FooterHero";
+import Footer from "@/components/Footer";
 
 async function getData() {
-  const res = await fetch("https://www.jsonkeeper.com/b/4G1G", {
-    cache: "force-cache",
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const exploreRes = await fetch("https://www.jsonkeeper.com/b/4G1G", {
+      cache: "force-cache",
+    });
+    const exploreData = await exploreRes.json();
+
+    const cardsRes = await fetch("https://www.jsonkeeper.com/b/VHHT", {
+      cache: "force-cache",
+    });
+    const cardsData = await cardsRes.json();
+
+    return { exploreData, cardsData };
+    
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { exploreData: [], cardsData: [] }; // Return empty arrays or appropriate default data
+  }
 }
 
+
 const Page = async () => {
-  const exploreData = await getData();
+  const {exploreData, cardsData} = await getData();
 
   return (
     <div className="">
@@ -47,10 +63,27 @@ const Page = async () => {
 
         <section>
         <h2 className="text-4xl font-semibold py-8"> Live Anywhere </h2>
-        
-
+        <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+          {cardsData?.map(({ img, title }) => (
+              <LiveAnywhereCards
+              key={img}
+              img={img}
+              title={title}
+              />
+            ))}
+        </div>
         </section>
+
+        <FooterHero
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
+        
       </main>
+
+      <Footer/>
     </div>
   );
 };
