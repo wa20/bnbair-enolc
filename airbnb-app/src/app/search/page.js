ba11a7ee2346352094cad686eb26b2129c82f910
wael <78425538+wa20@users.noreply.github.com>
2024-05-15 16:22:20 +0100

    updated search page to destructure url params

--- airbnb-app/src/app/search/page.js
+++ airbnb-app/src/app/search/page.js
@@ -4,10 +4,34 @@ import React from "react";
 import Link from "next/link";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams, usePathname } from "next/navigation"
+import { useEffect } from "react";
+import { format } from "date-fns"
+
  
 const Search = () => {
-  const router = useRouter()
+  const searchParams = useSearchParams();
+
+  // Extracting query parameters
+  const location = searchParams.get('location');
+  const startDate = searchParams.get('startDate');
+  const endDate = searchParams.get('endDate');
+  const guests = searchParams.get('guests'); // 'Guests' is case-sensitive
+
+  // Returning an object with the extracted parameters
+  const searchData = {
+    location,
+    startDate,
+    endDate,
+    guests,
+  };
+
+  console.log('location', searchData)
+
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
+  const formattedEndtDate = format(new Date(startDate), "dd MMMM yy")
+  const dateRange = `${formattedStartDate} - ${formattedEndtDate}`
 
   return (
     <div>
@@ -15,8 +39,8 @@ const Search = () => {
 
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">300+ stays | actual values input </p>
-          <h1 className="text-3xl font-semibold mb-6"> Stays in: Location</h1>
+          <p className="text-xs">300+ stays | {dateRange} | for {guests} guests </p>
+          <h1 className="text-3xl font-semibold mb-6"> Stays in: {location}</h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-4">
             <p className="btn">
