a9abd51bf4fb10e6f2bb27fcb016fba0dc7f1a45
wael <78425538+wa20@users.noreply.github.com>
2024-05-15 16:18:17 +0100

    Header page update

--- airbnb-app/src/components/Header.js
+++ airbnb-app/src/components/Header.js
@@ -1,16 +1,82 @@
+"use client";
+
 import Image from "next/image";
-import { 
+import Link from "next/link";
+import {
   MagnifyingGlassIcon,
   GlobeEuropeAfricaIcon,
   Bars3Icon,
   UserCircleIcon,
- } from "@heroicons/react/24/solid";
+  UsersIcon,
+} from "@heroicons/react/24/solid";
+import { useState, useEffect } from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useRouter } from "next/navigation";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const router = useRouter();
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const cancelInput = () => {
+    setSearchInput("");
+  };
+
+  const handleSearch = () => {
+    // NextJS 14 - next/navigation cannot pass query params and requeirs a string
+    router.push(`/search/?location=${[searchInput]}?startDate=${startDate.toISOString()}
+      ?endDate=${endDate.toISOString()}?Guests=${numberOfGuests}`)
+
+    // router.push({
+    //   pathname: "/search",
+    //   query: {
+    //     location: searchInput,
+    //     startDate: startDate.toISOString(),
+    //     endDate: endDate.toISOString(),
+    //     numberOfGuests
+    //   },
+    // });
+  };
+
+
+  // const handleSearch = () => {
+  //   const queryParams = {
+  //     location: searchInput,
+  //     startDate: startDate.toISOString(),
+  //     endDate: endDate.toISOString(),
+  //     numberOfGuests,
+  //   };
+
+  //   router.push({
+  //     pathname: '/search',
+  //     query: queryParams,
+  //   });
+  // };
+
+
+  
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left section */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto ">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto "
+      >
         <Image
           src="https://links.papareact.com/qd3"
           alt="airbnb Logo"
@@ -23,6 +89,8 @@ function Header() {
       {/* middle section */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="start your search..."
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
@@ -33,19 +101,45 @@ function Header() {
       {/* right section */}
       <div className="flex items-center justify-end text-gray-500 space-x-4">
         <p className="hidden md:inline">Become a host</p>
-        <GlobeEuropeAfricaIcon
-          className="h-6 cursor-pointer"
-        />
+        <GlobeEuropeAfricaIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-          <Bars3Icon
-            className="h-6 cursor-pointer"
-          />
-          <UserCircleIcon 
-            className="h-6 cursor-pointer"
-          />
+          <Bars3Icon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
-        
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-5">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              max={20}
+              className="w-12 pl-3 test-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button onClick={cancelInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={handleSearch} className="flex-grow text-red-500">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
