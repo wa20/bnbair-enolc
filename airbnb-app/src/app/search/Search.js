01c794ab9561d1e8071d087ff05f1d056388124f
wael <78425538+wa20@users.noreply.github.com>
2024-05-16 09:30:08 +0100

    additional formatting and code clean up on search file

--- airbnb-app/src/app/search/Search.js
+++ airbnb-app/src/app/search/Search.js
@@ -3,12 +3,12 @@
 import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { useSearchParams } from "next/navigation"
-import { useEffect } from "react";
+import InfoCard from "@/components/InfoCard";
+import { useSearchParams } from "next/navigation";
 import { format } from "date-fns"
 
  
-const Search = async ({search}) => {
+const Search = ({search}) => {
   const searchParams = useSearchParams();
   // const { searchResults } = await getData();
 
@@ -20,28 +20,16 @@ const Search = async ({search}) => {
 
   // Returning an object with the extracted parameters
   
-  const searchData = {
-    location,
-    startDate,
-    endDate,
-    guests,
-  };
-
-  console.log('location', searchData)
-
+  // const searchData = {
+  //   location,
+  //   startDate,
+  //   endDate,
+  //   guests,
+  // };
 
   const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
   const formattedEndtDate = format(new Date(endDate), "dd MMMM yy")
   const dateRange = `${formattedStartDate} - ${formattedEndtDate}`
-
-
-
-  
-  useEffect(() => {
-   console.log('search:* ', search)
-  }, [search])
-  
- 
 
   return (
     <div>
@@ -53,30 +41,32 @@ const Search = async ({search}) => {
           <h1 className="text-3xl font-semibold mb-6"> Stays in: {location}</h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-4">
-            <p className="btn">
-              Cancellation Flexibility
-            </p>
-            <p className="btn">
-              Type of place
-            </p>
-            <p className="btn">
-              Price
-            </p>
-            <p className="btn">
-              Rooms and Beds
-            </p>
-            <p className="btn">
-              More Filters
-            </p>
+            <p className="btn">Cancellation Flexibility</p>
+            <p className="btn">Type of place</p>
+            <p className="btn">Price</p>
+            <p className="btn">Rooms and Beds</p>
+            <p className="btn">More Filters</p>
           </div>
+
+          <div className="flex flex-col">
+            {search?.map(item => (
+              <InfoCard
+                key={item.img} 
+                img={item.img}
+                location={item.location}
+                title={item.title}
+                description={item.description}
+                star={item.star}
+                price={item.price}
+                total={item.total}
+              />
+            ))}
+          </div>
+
         </section>
-
-        <section></section>
       </main>
-
       <Footer />
     </div>
   );
 };
-
 export default Search;
