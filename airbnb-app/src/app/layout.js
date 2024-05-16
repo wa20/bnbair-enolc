f425e7a1122cd6e0b8c83fa8741b7e7130966bf1
wael <78425538+wa20@users.noreply.github.com>
2024-05-16 10:09:04 +0100

    package added, router.events commented out as it is deprecated on nextjs 14, to find workaround

--- airbnb-app/src/app/layout.js
+++ airbnb-app/src/app/layout.js
@@ -1,7 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ProgressBar from "@badrap/bar-of-progress"
+import Router from 'next/navigation';
 
 const inter = Inter({ subsets: ["latin"] });
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: 'z-50',
+  delay: 100,
+});
+
+// Router.events.on('routeChangeStart', progress.start)
+// Router.events.on('routeChangeComplete', progress.finish)
+// Router.events.on("routeChangeError", progress.finish)
 
 export const metadata = {
   title: "AirBnb",
