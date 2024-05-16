0cb61b5adffc10debaf2b135073e1fc7d0212ef3
wael <78425538+wa20@users.noreply.github.com>
2024-05-16 09:29:29 +0100

    added suspense to Search component and formatting on page file

--- airbnb-app/src/app/search/page.js
+++ airbnb-app/src/app/search/page.js
@@ -1,28 +1,28 @@
-
-import React from "react";
-import Search from "@/app/search/Search";
+import React, { Suspense } from "react";
+import Search from "./Search";
 
 async function getData() {
-    try {
-      const searchRes = await fetch("https://www.jsonkeeper.com/b/5NPS", {cache: 'no-store'});
-      const searchResults = await searchRes.json();
-  
-      return { searchResults };
-      
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return { searchResults: [], }; // Return empty arrays or appropriate default data
-    }
-    
+  try {
+    const searchRes = await fetch("https://www.jsonkeeper.com/b/5NPS", {
+      cache: "force-cache",
+    });
+    const searchResults = await searchRes.json();
+
+    return { searchResults };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { searchResults: [] }; // Return empty arrays or appropriate default data
   }
+}
 
 const Page = async () => {
-
   const { searchResults } = await getData();
 
   return (
     <div>
-     <Search search={searchResults} />
+      <Suspense>
+        <Search search={searchResults} />
+      </Suspense>
     </div>
   );
 };
